200457aa20caf13ead282b6e835243869d9f3246
bryan <btweinstein@gmail.com>
2015-10-26 14:58:18 -0400

    Trying to fix my code. Gotta go to lab meeting, I'll take a look there.

--- adr_2d/solver.py
+++ adr_2d/solver.py
@@ -5,7 +5,7 @@ import scipy as sp
 
 class Solver(object):
 
-    def __init__(self, imax=100, jmax=100, kmax=10, dt=0.01, dr=1.,
+    def __init__(self, imax=10, jmax=10, kmax=20, dt=0.01, dr=1.0,
                  u=None, v=None, D=5., s=1., fi_orig=None):
 
         self.imax = imax
@@ -27,16 +27,21 @@ class Solver(object):
         else:
             self.u = u
 
+        # Now convert u and v to logical indices! We are using a simple technique to do this; it could be time
+        # consuming to do this in general for an arbitrary index
+        self.v = self.v.ravel() # In c order automatically, which matches our logical indexing
+        self.u = self.u.ravel()
+
         self.D =  D
         self.s = s
         if fi_orig is None:
             self.fi_orig = np.zeros((imax, jmax), dtype=np.double)
-            self.fi_orig[imax/2, jmax/2] = 0.5
+            self.fi_orig[imax/2, jmax/2] = 0.1
         else:
             self.fi_orig = fi_orig
 
 
-        self.A = None
+        self.A = self.get_A()
         self.zeta = None
         self.I = None
         # self.setup_matrices()
@@ -46,16 +51,25 @@ class Solver(object):
 
         max_logical_index = self.get_logical_index(self.imax, self.jmax)
 
-        A = np.zeros((max_logical_index, max_logical_index), dtype=np.int) #TODO: Convert to sparse matrix!
+        A = np.zeros((max_logical_index, max_logical_index), dtype=np.double) #TODO: Convert to sparse matrix!
         for i in range(self.imax):
             for j in range(self.jmax):
+                uij = self.u[self.get_logical_index(i, j)]
+                vij = self.v[self.get_logical_index(i, j)]
 
+                first_term = (uij/(2.*self.dr))*(self.logical_dd(i, j, i+1, j) - self.logical_dd(i, j, i-1, j))
+                print self.logical_dd(i, j, i+1, j)
+                second_term = (vij/(2*self.dr))*(self.logical_dd(i,j,i,j+1) - self.logical_dd(i,j,i,j-1))
+                #print (vij/(2*self.dr))
+                A[i, j] = first_term + second_term
 
-    def logical_dd(self, i1, j1, i2, j2):
+        return A
+
+    def logical_dd(self, r, c, irow, jrow, icol, jcol):
         # A dirac delta that converts ij coordinates to logical index coordinates
-        log1 = self.get_logical_index(i1, j1)
-        log2 = self.get_logical_index(i2, j2)
-        if log1 == log2:
+        desired_row = self.get_logical_index(irow, jrow)
+        desired_col = self.get_logical_index(icol, jcol)
+        if (r == desired_row) and (c == desired_col):
             return 1
         else:
             return 0
